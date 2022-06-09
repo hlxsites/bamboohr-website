c699d458a394661c4bbbd0795bde3524850e3898
joshdwads <jwadsworth@bamboohr.com>
2022-06-09 14:58:46 -0600

    Just a start on the filter block

--- blog/blocks/filter/filter.js
+++ blog/blocks/filter/filter.js
@@ -1,12 +1,14 @@
 export default async function decorate(block) {
   block.textContent = '';
   let filters = await getFilters();
+
   let planTypes = uniqueFilters(filters, 'planType');
-  console.log(planTypes);
-  let productAreas = uniqueFilters(filters, 'productArea');
   planTypes.forEach((planType) => {
     block.append(createFilter(planType, "filterPlanType"));
   });
+
+  let productAreas = uniqueFilters(filters, 'productArea');
+
   productAreas.forEach((productArea) => {
     block.append(createFilter(productArea, "filterProductArea"));
   });
@@ -22,7 +24,7 @@ export function createFilter(filter, classPrefix) {
   const card = document.createElement('div');
   card.className = `${classPrefix}-card`;
   card.innerHTML = `<div class="${classPrefix}-header">
-    <input type="checkbox" class="${classPrefix}-plan-type" id="${filter}">${filter}</input> 
+    <input type="checkbox" class="${classPrefix}-plan-type" id="${filter.planType}">${filter.planType}</input> 
     </div>`;
   return (card);
 }
